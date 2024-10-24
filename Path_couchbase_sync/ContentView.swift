cdffac0dfa1a6b0eb12046ff8c5c04c4f6a60471
Karpahalakshmi Senthilkumar <sklsuja72@gmail.com>
2024-10-24 15:45:34 +0530

    Initial commit

--- Path_couchbase_sync/ContentView.swift
+++ Path_couchbase_sync/ContentView.swift
@@ -8,14 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var mrn = ""
+    @State private var firstname = ""
+    @State private var middlename = ""
+    @State private var lastname = ""
+    @State private var gender: String = "Male"
+    @State private var dobDate = Date.now
+    @State private var admitDate = Date.now
+    @State private var admissionNumber = ""
+    let genderList = ["Male", "Female"]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Form {
+            TextField("Medical Record Number(MRN)", text:$mrn)
+            Section(header: Text("Patient Details")) {
+                TextField("Patient First Name", text: $firstname)
+                TextField("Patient Middle Name", text: $middlename)
+                TextField("Patient Last Name", text: $lastname)
+            }
+            Picker("Gender", selection: $gender) {
+                ForEach(genderList, id: \.self) { gender in
+                    Text(gender).tag(gender)
+                }
+            }
+            DatePicker("DOB", selection: $dobDate, displayedComponents: .date)
+            TextField("Admission/Encounter Number", text: $admissionNumber)
+            DatePicker("Admit Date", selection: $admitDate, displayedComponents: .date)
         }
-        .padding()
     }
 }
 
---
+++ Path_couchbase_sync/models/PatientDetails.swift
@@ -0,0 +1,19 @@
+//
+//  PatientDetails.swift
+//  Path_couchbase_sync
+//
+//  Created by Karpahalakshmi on 24/10/24.
+//
+
+import Foundation
+
+struct PatientDetails {
+    var mrn: String?
+    var firstName: String?
+    var middleName: String?
+    var lastName: String?
+    var gender: String?
+    var dateOfBirth: Date?
+    var admissionNo: String?
+    var admitDate: Date?
+}
