601b612142eda8b589b51605ee701fc3eab59234
Karpahalakshmi Senthilkumar <sklsuja72@gmail.com>
2024-10-24 11:55:43 +0530

    Initial Commit

---
+++ Path_couchbase_sync/Path_couchbase_syncApp.swift
@@ -0,0 +1,17 @@
+//
+//  Path_couchbase_syncApp.swift
+//  Path_couchbase_sync
+//
+//  Created by Karpahalakshmi on 24/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Path_couchbase_syncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
